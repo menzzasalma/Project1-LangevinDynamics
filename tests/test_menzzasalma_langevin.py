2790043ab20c5c948f09016714cc29b59a94aba4
Patrick <pmenzzas@u.rochester.edu>
2018-10-03 22:39:29 -0400

    Added unit testing for every function with a numerical output

--- tests/test_menzzasalma_langevin.py
+++ tests/test_menzzasalma_langevin.py
@@ -36,3 +36,25 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+def testDragForce(response):
+    result = menzzasalma_langevin.dragForce(1,0.1) #testing simple multiplication
+    assert result == 0.1
+    result2 = menzzasalma_langevin.dragForce(1) #ensuring default value of input is accepted
+    assert result2 == 1
+
+def testRandomForce(response):
+    result = menzzasalma_langevin.randomForce(0.5) #if temperature is 0.5 and default values accepted, var = 1 and std = 1
+    assert result > -4 and result < 4 #with std of 1, normal distribution cenetered at zero should return a value between these bounds
+
+def testIntegrator(response):
+    result = menzzasalma_langevin.integrator(timeStep=1,velocity=0,position=0,temperature=0.5)
+    assert result[0] < 4 #if initial velocity is zero, then the final velocity is timeStep * randomForce. timeStep is 1, and randomForce should still be less than 4 if T=0.5
+    assert result[1] == 0 #the newPosition should be zero, as it starts at 0 and velocity is zero for the first timeStep 
+
+def testParticleMotion(response):
+    result = menzzasalma_langevin.particleMotion(1,1,0,0,300) #if the timeStep and timeTotal are equal, it will reach the max number of runs on the first go around, after adding just one set of items
+    assert len(result) == 2
+    result2 = menzzasalma_langevin.particleMotion(1,100,0,-1,300) #if the starting position is outside the bounds, it will notice after the first loop
+    assert len(result2) == 2
+
